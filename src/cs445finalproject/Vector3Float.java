0cf72e43a1c641981e5382af113ce024d4b13d0b
David <davidbaarns@hotmail.com>
2018-05-03 16:48:14 -0700

    Camera and camera controlls working

---
+++ src/cs445finalproject/Vector3Float.java
@@ -0,0 +1,29 @@
+/**
+ * *************************************************************
+ * file: Vector3Float.java
+ * author: David Baarns,
+ * class: CS 445 – Computer Graphics
+ *
+ * assignment: Final Project
+ * date last modified:
+ *
+ * purpose:
+ *
+ ***************************************************************
+ */
+
+
+
+package cs445finalproject;
+
+public class Vector3Float {
+
+    public float x, y, z;
+
+    public Vector3Float(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+}
--- src/cs445finalproject/cs445FinalProject.java
+++ src/cs445finalproject/cs445FinalProject.java
@@ -1,11 +1,11 @@
 /**
  * *************************************************************
  * file: cs445FinalProject.java
- * author: David Baarns, 
+ * author: David Baarns,
  * class: CS 445 – Computer Graphics
  *
  * assignment: Final Project
- * date last modified: 
+ * date last modified:
  *
  * purpose:
  *
@@ -13,21 +13,21 @@
  */
 package cs445finalproject;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
+import org.lwjgl.util.glu.GLU;
 
 public class cs445FinalProject {
-public float x, y, z;
 
- 
- 
+    private FPCameraController fp = new FPCameraController(0f, 0f, 0f);
+    private DisplayMode displayMode;
+
     public static void main(String[] args) {
+
         cs445FinalProject basic = new cs445FinalProject();
 
         basic.start();
@@ -36,11 +36,11 @@ public float x, y, z;
 
     // Calls method to create the windows, iniealize openGL,
     // and render
-    private void start() {
+    public void start() {
         try {
             createWindow();
             initGL();
-            render();
+            fp.gameLoop();//render();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -50,22 +50,27 @@ public float x, y, z;
     // Creates the new window and keyboard.
     private void createWindow() throws LWJGLException {
         Display.setFullscreen(false);
-
-        Display.setDisplayMode(new DisplayMode(640, 480));
+        DisplayMode d[]
+                = Display.getAvailableDisplayModes();
+        for (int i = 0; i < d.length; i++) {
+            if (d[i].getWidth() == 640
+                    && d[i].getHeight() == 480
+                    && d[i].getBitsPerPixel() == 32) {
+                displayMode = d[i];
+                break;
+            }
+        }
+        Display.setDisplayMode(displayMode);
         Display.setTitle("Final Project");
         Display.create();
-        //Keyboard.create();
-
     }
 
-    // Initializes openGL, sets window size and color
+// Initializes openGL, sets window size and color
     private void initGL() {
         glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-
-        glOrtho(0, 640, 0, 480, 1, -1);
+        GLU.gluPerspective(100.0f, (float) displayMode.getWidth() / (float) displayMode.getHeight(), 0.1f, 300.0f);
         glMatrixMode(GL_MODELVIEW);
         glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
     }
@@ -73,16 +78,15 @@ public float x, y, z;
     // This method prints instructions, reads the coordinates.txt file,
     // and handles the logic to color and draw each shape as needed.
     private void render() throws IOException {
-        
+
         while (!Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) {
             {
-               
-                }
-                Display.update();
-                Display.sync(60);
+
             }
+            Display.update();
+            Display.sync(60);
         }
-
-        //Display.destroy();
-
     }
+
+    //Display.destroy();
+}
